517d9fec4b91d0acbd06131cf168298de458c41f
PGabriel-MB <pablogrbarbosa@gmail.com>
2024-04-01 10:28:54 -0300

    adding another mongodb connection lib

--- database/User.py
+++ database/User.py
@@ -2,13 +2,14 @@ from flask_bcrypt import check_password_hash, generate_password_hash
 
 from .models import BaseDocument
 from .db import db
+from .Vehicle import Vehicle
 
 
 class User(BaseDocument):
     name = db.StringField(required=True, unique=True)
     email = db.EmailField(unique=True)
     password = db.StringField(required=True)
-    vehicles = db.ListField(db.EmbeddedDocumentField('Vehicle'))
+    vehicles = db.ListField(db.EmbeddedDocumentField(Vehicle))
 
     def set_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
--- database/db.py
+++ database/db.py
@@ -1,4 +1,4 @@
-from flask_mongoengine import MongoEngine
+from mongoengine import MongoEngine
 
 db = MongoEngine()
 
