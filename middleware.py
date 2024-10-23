0f2aa6d109fa1df5b568ae897744457f36f56177
PGabriel-MB <pablogrbarbosa@gmail.com>
2024-10-23 20:54:39 -0300

    adds a token middleware to be tested

---
+++ middleware.py
@@ -0,0 +1,36 @@
+from flask import jsonify
+from werkzeug.exceptions import HTTPException
+
+def register_jwt_handlers(app, jwt):
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(reason):
+        '''Invalid token handler'''
+        return jsonify({
+            "error": "Invalid Token!",
+            "message": reason
+        }), 401
+    
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        '''Expired token handler'''
+        return jsonify({
+            "error": "Expired Token!",
+            "message": "Please, log in again..."
+        }), 401
+
+    @jwt.unauthorized_loader
+    def unauthorized_callback(reason):
+        '''No token handler'''
+        return jsonify({
+            "error": "Authorization token required",
+            "message": reason
+        }), 401
+    
+    @app.errorHandler(HTTPException)
+    def handle_http_exception(error):
+        '''HTTP Exceptions handler'''
+        return jsonify({
+            "error": "Erro de solicitação.",
+            "message": error.description
+        }), error.code
